b8857b9ae24b990b98997e323d861c0844acab21
Danny Peck <dannytpeck@gmail.com>
2017-10-29 15:17:32 -0700

    Add action for selecting a calendar

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_CLIENTS = 'FETCH_CLIENTS';
 export const SELECT_CLIENT = 'SELECT_CLIENT';
+export const SELECT_CALENDAR = 'SELECT_CALENDAR';
 
 export function fetchClients() {
   const url = '../data/data.json';
@@ -17,5 +18,12 @@ export function selectClient(client) {
   return {
     type: SELECT_CLIENT,
     payload: client
+  };
+}
+
+export function selectCalendar(calendar) {
+  return {
+    type: SELECT_CALENDAR,
+    payload: calendar
   };
 }
