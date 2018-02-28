1feaab97de4fd1b9e5fd575c9b3a2352b9658858
Danny Peck <dannytpeck@gmail.com>
2018-02-28 14:33:41 -0800

    Filter calendar into different phases for rendering

--- src/components/edit_calendar.jsx
+++ src/components/edit_calendar.jsx
@@ -23,6 +23,33 @@ class EditCalendar extends Component {
 
   render() {
     const calendar = this.props.selectedCalendar;
+    const yearlong = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Yearlong';
+    });
+    const phase1 = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 1';
+    });
+    const phase1b = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 1B';
+    });
+    const phase2 = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 2';
+    });
+    const phase2b = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 2B';
+    });
+    const phase3 = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 3';
+    });
+    const phase3b = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 3B';
+    });
+    const phase4 = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 4';
+    });
+    const phase4b = calendar.filter(challenge => {
+      return challenge.fields.Phase === 'Phase 4B';
+    });
 
     return (
       <div className="add-calendar">
@@ -31,7 +58,7 @@ class EditCalendar extends Component {
         <ClientName selectedClient={this.props.selectedClient} />
 
         <div className="calendar-name-and-link">
-          <h4 className="calendar-name">{calendar.name}</h4>
+          <h4 className="calendar-name">{'Calendar_' + calendar[0].fields['Program Year']}</h4>
           <img className="calendar-link"
             type="image"
             src="images/icon_link.svg"
@@ -41,15 +68,15 @@ class EditCalendar extends Component {
         </div>
 
         <div className="calendar-accordion my-4 clear" id="accordion" role="tablist">
-          <AccordionCard title={'Yearlong'} id={'yearlong'} calendar={calendar} />
-          <AccordionCard title={'Phase 1'} id={'phase1'} calendar={calendar} />
-          <AccordionCard title={'Phase 1B'} id={'phase1b'} calendar={calendar} />
-          <AccordionCard title={'Phase 2'} id={'phase2'} calendar={calendar} />
-          <AccordionCard title={'Phase 2B'} id={'phase2b'} calendar={calendar} />
-          <AccordionCard title={'Phase 3'} id={'phase3'} calendar={calendar} />
-          <AccordionCard title={'Phase 3B'} id={'phase3b'} calendar={calendar} />
-          <AccordionCard title={'Phase 4'} id={'phase4'} calendar={calendar} />
-          <AccordionCard title={'Phase 4B'} id={'phase4b'} calendar={calendar} />
+          <AccordionCard title={'Yearlong'} id={'yearlong'} calendar={yearlong} />
+          <AccordionCard title={'Phase 1'} id={'phase1'} calendar={phase1} />
+          <AccordionCard title={'Phase 1B'} id={'phase1b'} calendar={phase1b} />
+          <AccordionCard title={'Phase 2'} id={'phase2'} calendar={phase2} />
+          <AccordionCard title={'Phase 2B'} id={'phase2b'} calendar={phase2b} />
+          <AccordionCard title={'Phase 3'} id={'phase3'} calendar={phase3} />
+          <AccordionCard title={'Phase 3B'} id={'phase3b'} calendar={phase3b} />
+          <AccordionCard title={'Phase 4'} id={'phase4'} calendar={phase4} />
+          <AccordionCard title={'Phase 4B'} id={'phase4b'} calendar={phase4b} />
         </div>
 
         <h5 className="point-total my-3">{this.state.points} Points</h5>
