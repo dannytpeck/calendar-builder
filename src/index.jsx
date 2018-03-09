895ca4668c5db2d9e412d2b452525f0d6e3418e8
Danny Peck <dannytpeck@gmail.com>
2018-03-09 08:45:24 -0800

    Update to latest version of React

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/app';
-import reducers from './reducers';
 
 ReactDOM.render(<App />, document.querySelector('.container'));
