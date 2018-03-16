ce056fce5d275ef8d38d072078f006ba1b225557
Danny Peck <dannytpeck@gmail.com>
2018-03-16 15:49:14 -0700

    Allow user to edit challenges row by row

--- src/components/accordion_card.jsx
+++ src/components/accordion_card.jsx
@@ -9,12 +9,20 @@ class AccordionCard extends Component {
     super(props);
 
     this.state = {
-      challenges: []
+      challenges: [],
+      editingChallenge: null
     };
+
+    this.renderRow = this.renderRow.bind(this);
   }
 
   editChallenge(challenge) {
-    this.props.handleEditChallengeClick();
+    // this.props.handleEditChallengeClick();
+    if (this.state.editingChallenge && this.state.editingChallenge.id === challenge.id) {
+      this.setState({ editingChallenge: null });
+    } else {
+      this.setState({ editingChallenge: challenge });
+    }
   }
 
   hpImage(category) {
@@ -38,11 +46,95 @@ class AccordionCard extends Component {
     }
   }
 
-  renderRow(challenge) {
-    const startDate = moment(challenge.fields['Start date']).format('L');
-    const endDate = moment(challenge.fields['End date']).format('L');
+  updateName(event, challenge) {
+    let updatedChallenge = { ...challenge };
+    updatedChallenge.fields['Name'] = event.target.value;
+    this.setState({
+      editingChallenge: updatedChallenge
+    });
+  }
 
-    if (challenge.fields) {
+  updateStartDate(event, challenge) {
+    const date = moment(event.target.value).format('YYYY-MM-DD');
+
+    if (date !== 'Invalid date') {
+      let updatedChallenge = { ...challenge };
+      updatedChallenge.fields['Start date'] = date;
+      this.setState({
+        editingChallenge: updatedChallenge
+      });
+    }
+  }
+
+  updateEndDate(event, challenge) {
+    const date = moment(event.target.value).format('YYYY-MM-DD');
+
+    if (date !== 'Invalid date') {
+      let updatedChallenge = { ...challenge };
+      updatedChallenge.fields['End date'] = date;
+      this.setState({
+        editingChallenge: updatedChallenge
+      });
+    }
+  }
+
+  updateTracking(event, challenge) {
+    let updatedChallenge = { ...challenge };
+    updatedChallenge.fields['Frequency'] = event.target.value;
+    this.setState({
+      editingChallenge: updatedChallenge
+    });
+  }
+
+  updatePoints(event, challenge) {
+    let updatedChallenge = { ...challenge };
+    updatedChallenge.fields['Points'] = event.target.value;
+    this.setState({
+      editingChallenge: updatedChallenge
+    });
+  }
+
+  renderRow(challenge) {
+    const startDate = moment(challenge.fields['Start date']).format('YYYY-MM-DD');
+    const endDate = moment(challenge.fields['End date']).format('YYYY-MM-DD');
+    const name = challenge.fields['Name'];
+    const points = challenge.fields['Points'];
+    const frequency = challenge.fields['Frequency'];
+
+    if (this.state.editingChallenge && this.state.editingChallenge.id === challenge.id) {
+      return (
+        <tr key={challenge.id} className="editing-row">
+          <td scope="row">
+            <input type="text" className="form-control" value={name} onChange={(e) => this.updateName(e, challenge)} />
+          </td>
+          <td>{challenge.fields['Required']}</td>
+          <td>{challenge.fields['Verified'] === 'No' ? 'Self-Report' : 'CIE'}</td>
+          <td className="category-cell">
+            <img className="table-icon" src={this.hpImage(challenge.fields['HP Element'])} />
+            <img className="table-icon" src={this.teamImage(challenge.fields['Team/Ix'])} />
+          </td>
+          <td>
+            <input type="date" className="form-control edit-date" value={startDate} onChange={(e) => this.updateStartDate(e, challenge)} />
+            <input type="date" className="form-control edit-date" value={endDate} onChange={(e) => this.updateEndDate(e, challenge)} />
+          </td>
+          <td>
+            <select className="form-control" value={frequency} onChange={(e) => this.updateTracking(e, challenge)}>
+              <option>One Time</option>
+              <option>Weekly</option>
+            </select>
+          </td>
+        <td>
+          <input type="text" className="form-control edit-points" value={points} onChange={(e) => this.updatePoints(e, challenge)} />
+          <span>({challenge.fields['Total Points']})</span>
+        </td>
+          <td className="actions-cell">
+            <img className="table-icon" src="images/icon_edit.svg" onClick={() => this.editChallenge(challenge)} />
+            <img className="table-icon" src="images/icon_comment.svg" />
+            <img className="table-icon" src="images/icon_delete.svg" />
+          </td>
+        </tr>
+      );
+    } else {
       return (
         <tr key={challenge.id}>
           <td scope="row">{challenge.fields['Name']}</td>
@@ -52,7 +144,7 @@ class AccordionCard extends Component {
             <img className="table-icon" src={this.hpImage(challenge.fields['HP Element'])} />
             <img className="table-icon" src={this.teamImage(challenge.fields['Team/Ix'])} />
           </td>
-          <td>{startDate} - {endDate}</td>
+          <td>{moment(startDate).format('L')} - {moment(endDate).format('L')}</td>
           <td>{challenge.fields['Frequency']}</td>
         <td>{challenge.fields['Points']} ({challenge.fields['Total Points']})</td>
           <td>
@@ -63,6 +155,8 @@ class AccordionCard extends Component {
         </tr>
       );
     }
+
+
   }
 
   render() {
