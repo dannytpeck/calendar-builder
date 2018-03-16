358444d5175d63445463470a244e063680530223
Danny Peck <dannytpeck@gmail.com>
2018-03-16 09:35:57 -0700

    Hook edit button up on ShowCalendar view to the DB

--- src/components/calendar_table.jsx
+++ src/components/calendar_table.jsx
@@ -27,17 +27,27 @@ class CalendarTable extends Component {
   }
 
   editCalendar(calendar) {
-    this.props.selectCalendar(calendar);
-    this.props.handleEditClick();
+    const employerName = calendar.fields.client;
+    const programYear = calendar.fields.year;
+    const url = `https://api.airtable.com/v0/appN1J6yscNwlzbzq/Challenges?api_key=keyCxnlep0bgotSrX&filterByFormula=AND({EmployerName}='${employerName}',{Program Year}='${programYear}')`;
+
+    axios.get(url)
+      .then(response => {
+        this.props.selectCalendar(response.data.records);
+        this.props.handleEditClick(response.data.records);
+      })
+      .catch(error => console.error(error));
   }
 
   renderRow(calendar) {
+    const { name, year, updated, status } = calendar.fields;
+
     return (
       <tr key={calendar.id}>
-        <td>{calendar.name}</td>
-        <td>{calendar.year}</td>
-        <td>{calendar.updated}</td>
-        <td>{calendar.status}</td>
+        <td>{name}</td>
+        <td>{year}</td>
+        <td>{updated}</td>
+        <td>{status}</td>
         <td>
           <img onClick={() => this.editCalendar(calendar)} className="edit-icon" src="images/icon_edit.svg" />
         </td>
@@ -50,7 +60,7 @@ class CalendarTable extends Component {
 
   render() {
     const filteredCalendars = this.state.calendars.filter(calendar => {
-      return calendar.fields.client[0] === this.props.selectedClient.id;
+      return calendar.fields.client === this.props.selectedClient.fields['Limeade e='];
     });
 
     return (
@@ -66,7 +76,7 @@ class CalendarTable extends Component {
           </tr>
         </thead>
         <tbody>
-          {filteredCalendars.map(calendar => this.renderRow(calendar.fields))}
+          {filteredCalendars.map(calendar => this.renderRow(calendar))}
         </tbody>
       </table>
     );
